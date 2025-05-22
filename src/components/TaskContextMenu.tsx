7bb0a176f3d8380f9ac22d20b8dbcf84db4a8425
Linus <contact@linusskold.com>
2025-05-23 01:15:16 +0200

    Fixed issue where context menu would render below other items

--- src/components/TaskContextMenu.tsx
+++ src/components/TaskContextMenu.tsx
@@ -5,55 +5,55 @@ import { Fragment } from 'react';
 
 const TaskContextMenu = () => {
   return (
-    <Menu>
-      <MenuButton>
-        {' '}
-        <EllipsisVerticalIcon className="h-5 w-5 text-gray-400 hover:text-gray-100" />
-      </MenuButton>
-      <MenuItems
-        transition
-        className="font-bold w-52 origin-top-right rounded-xl border border-white/5 bg-gray-900 p-2 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:--spacing(1)] focus:outline-none data-closed:scale-95 data-closed:opacity-0"
-        anchor="bottom end"
-      >
-        <MenuItem>
-          <a
-            className="block data-focus:bg-gray-800 px-2 py-1.5"
-            href="/settings"
-          >
-            Edit
-          </a>
-        </MenuItem>
-        <MenuItem>
-          <a
-            className="block data-focus:bg-gray-800 px-2 py-1.5"
-            href="/settings"
-          >
-            Start
-          </a>
-        </MenuItem>
-        <MenuItem>
-          <a
-            className="block data-focus:bg-gray-800 px-2 py-1.5"
-            href="/support"
-          >
-            Complete
-          </a>
-        </MenuItem>
-        <hr className="border-gray-500 my-2" />
-        <MenuItem>
-          <div className="flex items-center space-x-2">
-            <TrashIcon className="h-5 w-5 text-red-500" />
+      <Menu>
+        <MenuButton>
+          {' '}
+          <EllipsisVerticalIcon className="h-5 w-5 text-gray-400 hover:text-gray-100" />
+        </MenuButton>
+        <MenuItems
+          transition
+          className="z-50 font-bold w-52 origin-top-right rounded-xl border border-white/5 bg-gray-900 p-2 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:--spacing(1)] focus:outline-none data-closed:scale-95 data-closed:opacity-0"
+          anchor="bottom end"
+        >
+          <MenuItem>
             <a
-              className="block data-focus:bg-gray-800 text-red-500 px-2 py-1.5"
-              href="#"
-              onClick={() => console.log('Delete clicked')}
+              className="block data-focus:bg-gray-800 px-2 py-1.5"
+              href="/settings"
             >
-              Delete
+              Edit
             </a>
-          </div>
-        </MenuItem>
-      </MenuItems>
-    </Menu>
+          </MenuItem>
+          <MenuItem>
+            <a
+              className="block data-focus:bg-gray-800 px-2 py-1.5"
+              href="/settings"
+            >
+              Start
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              className="block data-focus:bg-gray-800 px-2 py-1.5"
+              href="/support"
+            >
+              Complete
+            </a>
+          </MenuItem>
+          <hr className="border-gray-500 my-2" />
+          <MenuItem>
+            <div className="flex items-center space-x-2">
+              <TrashIcon className="h-5 w-5 text-red-500" />
+              <a
+                className="block data-focus:bg-gray-800 text-red-500 px-2 py-1.5"
+                href="#"
+                onClick={() => console.log('Delete clicked')}
+              >
+                Delete
+              </a>
+            </div>
+          </MenuItem>
+        </MenuItems>
+      </Menu>
   );
 };
 
