d7d7864bbad856739c1f2c5b6853357b12c3c5e1
Linus <contact@linusskold.com>
2025-05-26 20:44:48 +0200

    Tried adding color randomness to chip component

--- src/components/ChipComponent.tsx
+++ src/components/ChipComponent.tsx
@@ -5,20 +5,37 @@ export const ChipComponentSchema = z.object({
   label: z.string(),
   onClick: z.function().args(z.unknown()).optional(),
   className: z.string().optional(),
+  index: z.number().default(0),
 });
 export type ChipComponentProps = z.infer<typeof ChipComponentSchema>;
 
 
+
+
 const ChipComponent = (props: ChipComponentProps) => {
+
+    const colors = [
+      "emerald",
+      "sky",
+      "rose",
+      "violet",
+      "amber",
+      "indigo",
+      "lime",
+    ];
   
-  const { label, onClick, className = "" } = props;
+  const { label, onClick, index, className = "" } = props;
   
+
+    const color = colors[index];
+
   return (
     <div
-      className={`${className} flex items-center px-2 py-1 border border-gray-300 rounded-full cursor-pointer hover:bg-gray-200`}
+      className={`${className} z-20 flex items-center px-2 py-1 border border-${color}-300 rounded-full cursor-pointer 
+      bg-${color}-900 hover:bg-${color}-600`}
       onClick={onClick}
     >
-      <span className="text-sm font-medium">{label}</span>
+      <span className="text-xs font-medium">{label}</span>
     </div>
   );
 }
