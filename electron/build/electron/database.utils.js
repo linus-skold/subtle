5484c582e8b7dfd5cae3025541ff6826e47c374b
Linus <contact@linusskold.com>
2025-06-11 01:45:46 +0200

    fixed basic ipc call, electronAPI now accessible in nextjs

--- electron/build/electron/database.utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// import * as sqlite from "../electron/database/sqlite.utils";
-// import * as subtask from "../electron/database/subtask.db.utils";
-// import * as task from "../electron/database/task.db.utils";
-// import * as taskList from "../electron/database/tasklist.db.utils";
-// export { taskList, task, subtask, sqlite };
-//# sourceMappingURL=database.utils.js.map
