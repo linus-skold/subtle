05eba96a885321968ab63234ac4674a6432a19b6
Linus <contact@linusskold.com>
2025-06-14 00:57:32 +0200

    remvoed dist from git

--- apps/next/dist/static/chunks/apps_next_src_app_layout_tsx_28500d5e._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules__pnpm_a07e0dd5._.js",
-  "static/chunks/[root-of-the-server]__a73b190a._.js",
-  "static/chunks/[root-of-the-server]__b1490140._.css"
-],
-    source: "dynamic"
-});
