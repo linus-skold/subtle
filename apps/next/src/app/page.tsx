d14bd923522743ba9352cd24ac0eb4673d04c447
Linus <contact@linusskold.com>
2025-06-28 01:01:49 +0200

    feat: implemented archiving completed tasks & uncompleting tasks

--- apps/next/src/app/page.tsx
+++ apps/next/src/app/page.tsx
@@ -57,13 +57,15 @@ function useWindowSize() {
 export default function Home() {
   const { appService, setSettings } = useAppContext();
 
-  const [ isSettingsModalOpen , setIsSettingsModalOpen ] = useState(false);
-  const [ isFocusMode , setIsFocusMode ] = useState(false);
+  const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false);
+  const [isFocusMode, setIsFocusMode] = useState(false);
 
   const [settingsOpen, setSettingsOpen] = useState(false);
   const [isStartup, setIsStartup] = useState(true);
 
   const [isEditingNote, setIsEditingNote] = useState(false);
+  const [hasCleared, setHasCleared] = useState(false);
+  const [clearedTasks, setClearedTasks] = useState<Task[]>([]);
 
   const [tasks, setTasks] = useState<Task[]>([]);
   const [activeTask, setActiveTask] = useState<Task>(null);
@@ -80,8 +82,8 @@ export default function Home() {
       try {
         taskContext.taskService
           .getTasks()
-          .then((fetchedTasks) => {
-            setTasks(fetchedTasks as Task[]);
+          .then((fetchedTasks: Task[]) => {
+            setTasks(fetchedTasks.filter((task) => !task.archived));
           })
           .catch((error: unknown) => {
             console.error("Failed to fetch tasks:", error);
@@ -95,22 +97,21 @@ export default function Home() {
       }
 
       try {
-              appService
-                .getSettings()
-                .then((fetchedSettings) => {
-                  console.log("Fetched settings:", fetchedSettings);
-                  setSettings(fetchedSettings as Setting[]);
-                })
-                .catch((error: unknown) => {
-                  console.error("Failed to fetch settings:", error);
-                });
-              setTimeout(() => {
-                // Simulate a delay for startup
-                setIsStartup(false);
-              }, 1000);
-            } catch (error) {
-              console.error("Error during database initialization:", error);
-            }
+        appService
+          .getSettings()
+          .then((fetchedSettings) => {
+            setSettings(fetchedSettings as Setting[]);
+          })
+          .catch((error: unknown) => {
+            console.error("Failed to fetch settings:", error);
+          });
+        setTimeout(() => {
+          // Simulate a delay for startup
+          setIsStartup(false);
+        }, 1000);
+      } catch (error) {
+        console.error("Error during database initialization:", error);
+      }
     };
 
     if (isStartup) {
@@ -149,6 +150,28 @@ export default function Home() {
     }
   }
 
+  function handleClearTasks() {
+    if (!hasCleared) {
+      const toClear = tasks.filter((task) => task.completed);
+
+      setClearedTasks(toClear);
+
+      setTasks((prevTasks) => prevTasks.filter((task) => !task.completed));
+
+      toClear.forEach((task) => {
+        updateTask({ ...task, archived: true });
+      });
+      setHasCleared(true);
+    } else {
+      setTasks((prevTasks) => [...prevTasks, ...clearedTasks]);
+      clearedTasks.forEach((task) => {
+        updateTask({ ...task, archived: false });
+      });
+      setClearedTasks([]);
+      setHasCleared(false);
+    }
+  }
+
   if (isStartup) {
     return (
       <div className="flex items-center justify-center h-screen">
@@ -176,8 +199,13 @@ export default function Home() {
   };
 
   const updateTask = (updatedTask: Task) => {
+    // check if we are completing a task
+    if (updatedTask.completed && !updatedTask.active) {
+      setHasCleared(false);
+    }
+
     taskContext.taskService
-      .updateTask({ ...updatedTask, completed: true })
+      .updateTask({ ...updatedTask })
       .then((task) => {
         setTasks((prevTasks) =>
           prevTasks.map((t) => (t.id === task.id ? task : t)),
@@ -189,14 +217,17 @@ export default function Home() {
   };
 
   const completeTask = (taskId: number) => {
-    updateTask(tasks.find((task) => task.id === taskId));
+    updateTask({
+      ...tasks.find((task) => task.id === taskId),
+      completed: true,
+    });
     setActiveTask(null);
   };
 
   const toggleNoteMode = () => {
     setSettingsOpen(false);
     const nextMode = !isEditingNote;
-    if( nextMode === false ) {
+    if (nextMode === false) {
       setIsEditingNote(false);
     }
     // updateState({ isNoteMode: !noteMode });
@@ -234,15 +265,21 @@ export default function Home() {
                 className="h-6 w-6 text-gray-400 hover:text-blue-500 transition-colors cursor-pointer"
                 onClick={toggleNoteMode}
               />
-              <Cog6ToothIcon className="h-6 w-6 text-gray-400 hover:text-blue-500 transition-colors cursor-pointer " onClick={() => setSettingsOpen(true)} />
+              <Cog6ToothIcon
+                className="h-6 w-6 text-gray-400 hover:text-blue-500 transition-colors cursor-pointer "
+                onClick={() => setSettingsOpen(true)}
+              />
             </div>
 
             <SlideoutComponent isOpen={isEditingNote} onClose={toggleNoteMode}>
               <NotesComponent />
             </SlideoutComponent>
 
-            <SlideoutComponent isOpen={settingsOpen} onClose={() => setSettingsOpen(false)}>
-              <SettingsModal /> 
+            <SlideoutComponent
+              isOpen={settingsOpen}
+              onClose={() => setSettingsOpen(false)}
+            >
+              <SettingsModal />
             </SlideoutComponent>
 
             <div className="flex flex-col flex-1 h-screen gap-4 p-4 overflow-auto">
@@ -295,19 +332,46 @@ export default function Home() {
               >
                 <div className="h-[2px] mx-12 bg-gradient-to-r from-green-400 to-blue-500 border-0 rounded-full" />
               </AddTaskComponent>
+
+              {clearedTasks.length > 0 && (
+                <div
+                  className="text-gray-400 hover:text-gray-300 text-sm text-center"
+                  onClick={() => handleClearTasks()}
+                >
+                  {!hasCleared && <span>Clear list</span>}
+                  {hasCleared && <span>Undo</span>}
+                </div>
+              )}
+
+              {clearedTasks.length <= 0 && (
+                <div className="text-gray-400 hover:text-gray-300 text-sm text-center">
+                  <span>All caught up</span>
+                </div>
+              )}
+
               <TaskList className="flex-[1_1_25%]">
                 {tasks.length > 0 &&
                   tasks
                     .filter((task) => task.completed)
-                    .map((task) => <CompletedTask key={task.id} task={task} />)}
+                    .map((task) => (
+                      <CompletedTask
+                        key={task.id}
+                        task={task}
+                        onArchive={() => updateTask({ ...task, archived: true })}
+                        onUncomplete={() =>
+                          updateTask({ ...task, completed: false })
+                        }
+                      />
+                    ))}
               </TaskList>
+
               <div className="w-full h-8 pb-4 bg-[var(--background)] flex items-center justify-center">
-                <p className="text-sm">{version}</p>
+                <p className="text-sm">
+                  Version: <span className="text-blue-400">{version}</span>
+                </p>
               </div>
             </div>
-
           </div>
-
         </main>
       </SortableContext>
     </DndContext>
--- apps/next/src/components/CompletedTask.tsx
+++ apps/next/src/components/CompletedTask.tsx
@@ -2,8 +2,17 @@ import React, { useState } from "react";
 import type { Task } from "@db/schema/task.schema";
 import { formatProgress } from "../utils/time.utils";
 import { ArchiveBoxArrowDownIcon } from "@heroicons/react/24/outline";
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
 
-const CompletedTask = ({ task }: { task: Task }) => {
+const CompletedTask = ({
+  task,
+  onArchive,
+  onUncomplete,
+}: {
+  task: Task;
+  onArchive: (task: Task) => void;
+  onUncomplete: (task: Task) => void;
+}) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
@@ -12,7 +21,20 @@ const CompletedTask = ({ task }: { task: Task }) => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <span className="text-sm line-through text-gray-500">{task.title}</span>
+      <div className="flex items-center space-x-2 min-w-0 flex-grow">
+        <div
+          className={`overflow-hidden transition-all duration-100 ease-in-out z-50 ${
+            isHovered ? "w-5" : "w-0"
+          } flex-shrink-0`}
+        >
+          <CheckCircleIcon
+            className="h-5 w-5 hover:text-gray-400 text-green-500 transition-colors"
+            onClick={() => onUncomplete(task)}
+          />
+        </div>
+
+        <span className="text-sm line-through text-gray-500">{task.title}</span>
+      </div>
 
       <div className="flex items-center justify-end w-24 relative">
         {/* Progress */}
@@ -29,9 +51,7 @@ const CompletedTask = ({ task }: { task: Task }) => {
           className={`h-5 w-5 text-gray-400 hover:text-blue-500 transition-opacity duration-200 cursor-pointer absolute right-0 ${
             isHovered ? "opacity-100" : "opacity-0 pointer-events-none"
           }`}
-          onClick={() => {
-            console.log(`Archiving task: ${task.id}`);
-          }}
+          onClick={() => onArchive(task)}
         />
       </div>
     </div>
